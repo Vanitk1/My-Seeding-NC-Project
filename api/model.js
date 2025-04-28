6d88fc11e94304431f4a33d586fd92d72a2cd63d
Vanit Kashab <vanitk1@gmail.com>
2025-04-28 16:28:26 +0100

    question 3 completed

--- api/model.js
+++ api/model.js
@@ -3,6 +3,9 @@ const db = require("../db/connection");
 exports.selectTopics = () => {
     return db.query(`SELECT slug, description FROM topics;`)
     .then(({ rows }) => {
+        // if (rows.length === 0) {
+        //     return Promise.reject ({ status: 404, msg: "Not found" });
+        // }
         return rows;
     })
 }
