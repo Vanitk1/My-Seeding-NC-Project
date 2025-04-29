52677e7c1604bae7b9c2351ceb8467df5b8d50d3
Vanit Kashab <vanitk1@gmail.com>
2025-04-29 01:03:25 +0100

    question 5 completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const seed = require('../db/seeds/seed')
 const data = require('../db/data/development-data');
 const request = require("supertest");
 const app = require("../api/app");
+const { toBeSortedBy } = require("jest-sorted");
 //const endpoints = require("../endpoints.json")
 
 
@@ -89,6 +90,87 @@ describe("GET /api/articles/:article_id", () => {
     .expect(404)
     .then(({ body }) => {
       expect(body.msg).toBe("Not found")
-    })
-  })
+    });
+  });
+});
+
+describe("GET /api/articles", () => {
+  test("200, checks for an array of articles objects", () => {
+    return request(app)
+    .get("/api/articles")
+    .expect(200)
+    .then(({ body: {articles} }) => {
+      expect(Array.isArray(articles)).toBe(true)
+    });
+  });
+
+  test("200, checks to see if the article object has no body property", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(Number)
+            }));
+          expect(article).not.toHaveProperty("body");
+        });
+      });     
+    });
+  
+  test("200, articles are sorted in descending order by date", () => {
+    return request(app)
+    .get("/api/articles")
+    .expect(200)
+    .then(({ body: { articles } }) => {
+      expect(articles).toBeSortedBy("created_at", {descending: true}) // created_at is the column date
+    });
+  });
+});
+
+describe("GET /api/articles/:article_id/comments", () => {
+  test("checks for an array of comments for the given article_id", () => {
+    return request(app)
+    .get("/api/articles/1/comments")
+    .expect(200)
+    .then(({ body: { comments } }) => {
+      comments.forEach((comment) => {
+        expect(comment).toEqual(
+          expect.objectContaining({
+            comment_id: expect.any(Number),
+            votes: expect.any(Number),
+            created_at: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            article_id: expect.any(Number)
+          }));
+      });
+    }); 
+  });
+
+  test("200, checks for an empty array if article exists but has no comments", () => {
+    return request(app) 
+    .get("/api/articles/100/comments")
+    .expect(200)
+    .then(({ body: { comments } }) => {
+    expect(comments).toEqual([]);
+    });
+  });
+
+  test("400: returns bad request when number is given as a word instead", () => {
+    return request(app)
+      .get("/api/articles/not-a-number/comments")
+      .expect(400)
+      .then(({ body }) => {
+      expect(body.msg).toBe("bad request");
+    });
+  });
 });
--- api/controller.js
+++ api/controller.js
@@ -1,6 +1,7 @@
-const { selectTopics, selectArticleById } = require("../api/model");
+const { selectTopics, selectArticleById, selectArticles, selectCommentsByArticleId } = require("../api/model");
 const endpoints = require("../endpoints.json");
 const db = require("../db/connection");
+const articles = require("../db/data/development-data/articles");
 
 exports.getEndPoints = (req, res, next) => {
     res.status(200).send({ endpoints })
@@ -21,7 +22,24 @@ exports.getArticleId = (req, res, next) => {
     .then((article) => {
         res.status(200).send({article})
     })
-    .catch(next)
-
+    .catch(next);
 };
+
+exports.getArticles = (req, res, next) => {
+    selectArticles()
+    .then((articles) => {
+        res.status(200).send({ articles })
+    })
+    .catch(next)
+}
+
+exports.getCommentsByArticleId = (req, res, next) => {
+    const { article_id } = req.params;
+
+    selectCommentsByArticleId(article_id)
+    .then((comments) => {
+        res.status(200).send({ comments })
+    })
+    .catch(next);
+}
 //module.exports = {}
