a1beea1602bc4988106d89a7420405433cfe07cb
Vanit Kashab <vanitk1@gmail.com>
2025-05-19 09:23:45 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-//const http = require("http")
 const db = require("./db/connection");
 const router = require("./api/router");
-//const { getEndPoints, getTopics, getArticleId, getArticles, getCommentsByArticleId, postCommentsByArticleId, patchArticlesId, deleteCommentId, getUsers } = require("./api/controller");
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 
