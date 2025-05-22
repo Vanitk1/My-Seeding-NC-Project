f97722b6ae1d7c058494c033eadee1c67d8f6b3f
Vanit Kashab <vanitk1@gmail.com>
2025-05-22 11:19:36 +0100

    mistake fixed

--- db/data/development-data/topics.js
+++ db/data/development-data/topics.js
@@ -1,16 +1,16 @@
 module.exports = [
   { description: "Code is love, code is life",
-     slug: "Coding", 
+     slug: "coding", 
      img_url: "" },
   {
     description: "FOOTIE!",
-    slug: "Football",
+    slug: "football",
     img_url:
       "https://images.pexels.com/photos/209841/pexels-photo-209841.jpeg?w=700&h=700",
   },
   {
     description: "Hey good looking, what you got cooking?",
-    slug: "Cooking",
+    slug: "cooking",
     img_url:
       "https://images.pexels.com/photos/33242/cooking-ingredient-cuisine-kitchen.jpg?w=700&h=700",
   },
