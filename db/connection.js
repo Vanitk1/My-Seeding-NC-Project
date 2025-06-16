a1d10a34f6d7aee42532903ffa919397e5cb7950
Vanit Kashab <95109909+Vanitk1@users.noreply.github.com>
2025-06-17 00:44:31 +0100

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,26 @@
 const { Pool } = require("pg");
-const path = require('path');
+const path = require("path");
 
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
+if (ENV !== 'production') {
+  require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
+}
 
 const config = {};
 
 if (ENV === "production") {
-    config.connectionString = process.env.DATABASE_URL;
-    config.ssl = { rejectUnauthorized: false }; // 🔐 Enable SSL for Render DB
-    config.max = 2;
+  config.connectionString = process.env.DATABASE_URL;
+  config.ssl = { rejectUnauthorized: false };
+  config.max = 2;
 }
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error("No PGDATABASE or DATABASE_URL configured");
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
 } else if (process.env.DATABASE_URL) {
-    console.log(`Connected to ${process.env.DATABASE_URL}`);
+  console.log(`Connected to ${process.env.DATABASE_URL}`);
 } else {
-    console.log(`Connected to ${process.env.PGDATABASE}`);
+  console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
 const db = new Pool(config);
